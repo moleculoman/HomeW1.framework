6df6aac3be14a4791a22f311b5f9512aabcd57b4
moleculoman <Xz2@ZBS@mhSFpg7>
2023-03-03 22:05:13 +0700

    +

--- src/main/java/me/max/recipesite/controllers/FirstController.java
+++ src/main/java/me/max/recipesite/controllers/FirstController.java
@@ -1,26 +1,20 @@
 package me.max.recipesite.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.time.LocalDateTime;
 
-class Person {
-    public String name = "Maxim";
-    public String projectName = "RecipeSiteAPP";
-    public String LocalDateTime;
-    public String description = "Сайт с рецептами";
-
     @RestController
     public class FirstController {
-
+    @RequestMapping
         @GetMapping("/")
         public String helloWeb() {
             return "Приложение запущено";
         }
-
         @GetMapping("/info")
         public String oneStudent() {
-            return name + " " + projectName + " "+ LocalDateTime+ " " + description;
+            return "Maxim," + " RecipeSiteAPP," + " Сайт с рецептами";
         }
     }
-}
